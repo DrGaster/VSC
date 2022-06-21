96f6803d05178d20e4ac4d1b2f59843c6be3135b
Glen Nguyen <flammastnguyen@gmail.com>
2022-06-21 01:28:33 -0500

    Finished IfElse part 2

---
+++ Siblings.py
@@ -0,0 +1,8 @@
+name = input("Name? ")
+
+Siblings = ["Gary", "gary", "Glen", "glen", "Gina", "gina"]
+
+if name in Siblings:
+    print(name + " is a sibling!")
+else:
+    print(name + " is not a sibling...")
