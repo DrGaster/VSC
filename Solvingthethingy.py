d9dd83bccbc21cf92f2c18091d7fbec7b1afb2eb
Glen Nguyen <flammastnguyen@gmail.com>
2022-06-21 00:45:46 -0500

    renamed variables

--- Solvingthethingy.py
+++ Solvingthethingy.py
@@ -1,8 +1,8 @@
-NumVar1 = int(input("What is the First Number?  "))
-NumVar2 = int(input("What is the Second Number? "))
+NumVar1 = int(input("What is variable X? "))
+NumVar2 = int(input("What is variable Y? "))
 
 def expr(x, y):
-    return ((x + y)**2)-((y - x)**3)
+    return (x + y)**2 - (y - x)**3
 
 product = expr(NumVar1, NumVar2)
 print(product)
