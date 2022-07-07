67b0dc477995df02e8905737e30020e0bb4d7dbf
Glen Nguyen <flammastnguyen@gmail.com>
2022-07-07 18:35:39 -0500

    AAAA errors everywhere

---
+++ Minecraft_Modding/TutorialMod/src/main/java/com/xtalegaster/tutorialmod/proxy/ClientProxy.java
@@ -0,0 +1,5 @@
+package com.xtalegaster.tutorialmod.proxy;
+
+public class ClientProxy extends CommonProxy{
+
+}
---
+++ Minecraft_Modding/TutorialMod/src/main/java/com/xtalegaster/tutorialmod/util/Reference.java
@@ -0,0 +1,11 @@
+package com.xtalegaster.tutorialmod.util;
+
+public class Reference {
+
+	public static final String 	MOD_ID = "tutorialmo";
+	public static final String NAME = "Tutorial Mod";
+	public static final String VERSION = "0.0";
+	public static final String 	ACCEPTED_VERSIONS = "[1.12.2]";
+	public static final String CLIENT_PROXY_CLASS = "com.xtalegaster.tutorialmod.proxy.ClientProxy";
+	public static final String COMMON_PROXY_CLASS = "com.xtalegaster.tutorialmod.proxy.CommonProxy";
+}
