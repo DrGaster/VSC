2f9f11338bf7ccf8588181a1814553b3a00cf106
Glen Nguyen <flammastnguyen@gmail.com>
2022-07-08 13:06:21 -0500

    Clean Working Area

--- Minecraft_Modding/TutorialMod/src/main/java/com/xtalegaster/tutorialmod/proxy/CommonProxy.java
+++
@@ -1,5 +0,0 @@
-package bbxtalegaster.tutorialmod.proxy;
-
-public class CommonProxy {
-
-}
--- Python Folders/Lesson_1/Solvingthethingy.py
+++
@@ -1,8 +0,0 @@
-NumVar1 = int(input("What is variable X? "))
-NumVar2 = int(input("What is variable Y? "))
-
-def expr(x, y):
-    return (x + y)**2 - (y - x)**3
-
-product = expr(NumVar1, NumVar2)
-print(product)
